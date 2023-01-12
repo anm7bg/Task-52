1b6ed5fa127081e8b5256ce2a22936ec61e10115
A. Maslarov <nsite2014@gmail.com>
2023-01-12 13:47:52 +0200

    edit

--- src/js/task.js
+++ src/js/task.js
@@ -1,15 +1,25 @@
 const element = document.body;
 console.log("work");
+const a = document.getElementsByClassName("message");
+for (let index = 0; index < a.length; index++) {
+  a[index].style.display="none";
+  
+}
+// a.style.display = "none";
 // element.onclick = function() {
 //     alert('Thank you');
 //   };
 element.onclick = function() {
-    element.innerHTML += 
-    `
-    <article class="message">This is article 1</article>
-    <article class="message">This is article 2</article>
-    <article class="message">This is article 3</article>
-    <article class="message">This is article 4</article>
-    <article class="message">This is article 5</article>
-    `
+  for (let index = 0; index < a.length; index++) {
+    a[index].style.display="block";
+    
+  }
+    // element.innerHTML += 
+    // `
+    // <article class="message">This is article 1</article>
+    // <article class="message">This is article 2</article>
+    // <article class="message">This is article 3</article>
+    // <article class="message">This is article 4</article>
+    // <article class="message">This is article 5</article>
+    // `
   };
